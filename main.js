362a87b16e0972a2ea859656b7bb6ab92d9f97b2
[Copelia] <iamcorallium@gmail.com>
2018-08-18 12:53:50 -0500

    Añadiendo input con función

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData('text');
     ev.target.appendChild(document.getElementById(data));
   }
+
+
+  let createInput = document.getElementById('newInput');
+  let text = document.getElementById('text').value;
+
+  const newInput = () => {
+
+    // var x = document.createElement("TEXTAREA");
+    // var t = document.createTextNode("What is your main focus today?");
+    // x.appendChild(t);
+    // document.body.appendChild(x);
+
+   createInput.innerHTML = `<div class="input-group" id="drag-d" draggable="true" ondragstart="drag(event)">
+   <textarea oninput="newInput()" class="form-control" aria-label="With textarea"></textarea>
+ </div> `
+  }
